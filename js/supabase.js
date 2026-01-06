1a18ce0d609f1c30a6cf4775419e6301931a89db
thehub-com <korada209@gmail.com>
2026-01-06 09:18:04 +0200

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,4 @@
+const supabase = window.supabase.createClient(
+  'https://zdmtwnvaksdbvutrpcnr.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkbXR3bnZha3NkYnZ1dHJwY25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1Mjg4NjcsImV4cCI6MjA4MzEwNDg2N30.QztruYbzPeF8CrZmT_FhMw6VHc1-289qqJ8Qs4Z7nVc'
+);
