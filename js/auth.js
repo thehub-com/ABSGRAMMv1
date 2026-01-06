03a25488d373e71e476bbfdea295d316e7512bf0
thehub-com <korada209@gmail.com>
2026-01-06 09:32:12 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,37 +1,38 @@
-let tempEmail = '';
+const supabase = window.supabase.createClient(
+  'https://zdmtwnvaksdbvutrpcnr.supabase.co', // твой URL
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkbXR3bnZha3NkYnZ1dHJwY25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1Mjg4NjcsImV4cCI6MjA4MzEwNDg2N30.QztruYbzPeF8CrZmT_FhMw6VHc1-289qqJ8Qs4Z7nVc'               // anon public key
+);
 
+// splash → auth
+setTimeout(() => {
+  document.querySelector('.splash-container').style.display = 'none';
+  document.getElementById('authScreen').style.display = 'flex';
+}, 6000);
+
+// EMAIL → OTP
 async function sendCode() {
-  tempEmail = emailInput.value.trim();
+  const email = document.getElementById('emailInput').value;
+  if (!email) return alert('Введите email');
 
-  if (!tempEmail) {
-    alert('Введите email');
-    return;
-  }
-
-  const { error } = await supabase.auth.signInWithOtp({
-    email: tempEmail
-  });
+  const { error } = await supabase.auth.signInWithOtp({ email });
 
   if (error) {
     alert(error.message);
     return;
   }
 
-  authEmail.classList.add('hidden');
-  authCode.classList.remove('hidden');
+  document.getElementById('authEmail').style.display = 'none';
+  document.getElementById('authCode').style.display = 'block';
 }
 
+// VERIFY CODE
 async function verifyCode() {
-  const code = codeInput.value.trim();
-
-  if (!code) {
-    alert('Введите код');
-    return;
-  }
+  const email = document.getElementById('emailInput').value;
+  const token = document.getElementById('codeInput').value;
 
   const { error } = await supabase.auth.verifyOtp({
-    email: tempEmail,
-    token: code,
+    email,
+    token,
     type: 'email'
   });
 
@@ -40,5 +41,5 @@ async function verifyCode() {
     return;
   }
 
-  alert('Вы вошли ✅');
+  alert('Вы вошли');
 }
